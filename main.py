7179e5bd7a8d6435f33c4c5cc229263bc167f029
Anandha Kannan N <anandha.kannan@ascendo.ai>
2024-08-22 13:58:45 +0530

    Update main

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from services.assignment_services import setup_secret_santa
 
 secret_santa = SecretSantaAssignment()
 
-new_assignment = setup_secret_santa(
+setup_secret_santa(
     secret_santa=secret_santa,
     employee_uri="input/Employee-List.xlsx",
     previous_assignment_uri="input/Secret-Santa-Game-Result-2023.xlsx",
+    new_assignment_uri="output/Secret-Santa-Game-Result.xlsx",
 )
-print(new_assignment)
